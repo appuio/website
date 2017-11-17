d37233c454e5bbf633809c4090f44fcc30375c96
Damian Senn <senn@puzzle.ch>
2017-11-17 09:56:00 +0100

    Use new slider data

--- node-deps.sh
+++ node-deps.sh
@@ -2,5 +2,6 @@
 
 yarn install
 DEST=tmp/price-calculator/javascripts
+rm -rf $DEST
 mkdir -p $DEST
-cp node_modules/price-calculator/dist/price-calculator-1.0.0.min.js $DEST
+cp node_modules/price-calculator/dist/price-calculator-2.0.1.min.js $DEST
