36d65ff1911b40278bb3eb428dfbb3b08ff9f0d7
Martin Hertig <martin.hertig@gmail.com>
2019-10-28 15:15:46 +0100

    simplify beer-button.php

--- source/beer-button.php
+++ source/beer-button.php
@@ -1,29 +1,16 @@
 <?php
-// function createLogMessage($subject, $from_email, $message, $message_back){
-//     $log = "*********************************\n";
-//     $log .= "Date: " . date("Y-m-d H:i:s") . "\n";
-//     $log .= "From: " . $from_email . "\n";
-//     $log .= "Subject: " . $subject . "\n";
-//     $log .= "Message: " . $message . "\n";
-//     $log .= "MessageBack: " . $message_back . "\n";
-//     $log .= "*********************************\n";
-//     return $log;
-// }
 
-$message = isset($_POST['beerEmail']) ? $_POST['beerEmail'] : false;
+$message = isset($_GET['beerEmail']) ? $_GET['beerEmail'] : false;
+
 
 if($message != false) {
     $to = "hertig@puzzle.ch";
     $subject = "Someone wants to drink a beer!";
     $message = $message;
-    $success = sendMail($to, $subject, $message);
-    echo $success;
+    mail($to, $subject, $message, "From: $message");
+    echo "1";
+} else {
+  echo "no email provided";
 }
 
-function sendMail($toEmail, $subject, $message) {
-    $validFromEmail = spamcheck($fromEmail);
-    if($validFromEmail) {
-        mail($toEmail, $subject, $message, "From: $message");
-    }
-    return 1;
-}
+?>
