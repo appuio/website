32ecbde1e4df58e206c11a918750e37ae533539b
Julian Infanger <julian.infanger@gmail.com>
2020-06-30 10:03:02 +0200

    Revert "Toggle offer list on mobile devices"
    This reverts commit 050b71e008c894cf327a25ef6e854427356aed9e.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -38,12 +38,6 @@
        }
     });
 
-    // toggle-offer-feature
-    $('.toggle-offer-feature').click(function(event) {
-        event.preventDefault();
-        $(this).parents('.callout-item').toggleClass('visible');
-    })
-
 
 // Enable Submit button when terms and an offer is checked
     var checkboxes = $("input[name='terms']"),
@@ -235,6 +229,7 @@
     });
 
     window.onclick = function(e) {
+        this.console.log(e);
         if (!e.target.matches('.dropbtn-offerings') && !e.target.parentNode.matches('.dropbtn-offerings')) {
             const offeringDropdown = document.getElementById("offeringDropdown");
             offeringDropdown.classList.remove('show');
